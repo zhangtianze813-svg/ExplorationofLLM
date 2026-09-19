0cb186a317777c7d9f5c0b736acab61ce84f9932
zhangtianze813-svg <zhangtianze813@gmail.com>
2026-09-19 17:57:05 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# 检查 GPU 是否可用
-if torch.cuda.is_available():
-    # 获取当前 GPU 的名称
-    gpu_name = torch.cuda.get_device_name(0)
-    print(f"检测到 GPU 型号: {gpu_name}")
-else:
-    print("未检测到可用的 NVIDIA GPU，当前运行在 CPU 上。")
